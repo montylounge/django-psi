c7a769f9f2be83168ef4ea51d3c6121c8f8417e7
kevin <kevin@montylounge.com>
2014-07-07 01:44:45 -0400

    working on cleaning things up

---
+++ psi/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+VERSION = __import__('launchpad').__version__
+
+setup(name='django-psi',
+    version=VERSION,
+    description='Google Pagespeed Insights for your Django project.',
+    author='Kevin Fricovsky',
+    author_email='kevin@montylounge.com',
+    url='https://github.com/montylounge/django-psi/',
+    packages=find_packages(),
+    install_requires=['Django>=1.6.5','google-api-python-client'],
+    classifiers=[
+        'Development Status :: Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
