535b58b5026555f3ed17cdd7581c2601ef0ecdd3
kevin <kevin@montylounge.com>
2014-07-08 21:33:32 -0400

    updates to setup for pypi release and related .gitignore update

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-#!/usr/bin/env python
+import os
 from setuptools import setup, find_packages
 
 VERSION = __import__('psi').__version__
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(name='django-psi',
     version=VERSION,
     description='Google Pagespeed Insights for your Django project.',
+    long_description = read('README.md'),
     author='Kevin Fricovsky',
     author_email='kevin@montylounge.com',
     url='https://github.com/montylounge/django-psi/',
@@ -20,6 +24,5 @@ setup(name='django-psi',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ]
+    ],
 )
